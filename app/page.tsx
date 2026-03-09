5c03408e4740daaa083877c7b4f0c282a40be6bd
Era May Abelgas <era.abelgas@gmail.com>
2026-03-09 14:58:03 +0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -70,3 +70,20 @@ export default function LoginPage() {
   );
 
 }
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
+  )
+}
